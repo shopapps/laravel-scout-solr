01f05a47610bee6bbc07a823991c2c5d79ab5f19
Paul <paul@shopapps.co.uk>
2023-05-10 09:42:47 +0100

    Update SolrProvider.php
    fixes for registration of solrEngine

--- src/SolrProvider.php
+++ src/SolrProvider.php
@@ -4,6 +4,7 @@ namespace ScoutEngines\Solr;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Scout\EngineManager;
+use Laravel\Scout\Scout;
 use Solarium\Client;
 
 class SolrProvider extends ServiceProvider
@@ -14,18 +15,8 @@ class SolrProvider extends ServiceProvider
     public function boot()
     {
         app(EngineManager::class)->extend('solr', function ($app) {
-            $config = [
-                'endpoint' => [
-                    'default' => [
-                        'host' => config('scout.solr.host'),
-                        'port' => config('scout.solr.port'),
-                        'path' => config('scout.solr.path'),
-                        'core' => config('scout.solr.core'),
-                    ],
-                ],
-            ];
-
-            return new SolrEngine(new Client($config));
+            return new SolrEngine;
         });
     }
+
 }
